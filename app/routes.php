7c35706446782516e74ace78b6fb99d0b489a3af
Johanna Bodnyk <bodnyk@gmail.com>
2014-11-16 10:39:42 -0500

    new tree image

--- app/routes.php
+++ app/routes.php
@@ -21,10 +21,6 @@ Route::get('/', function()
         ->with('total', $total);
 });
 
-Route::get('envtest', function() {
-    echo $_ENV['CONNECTIONS'];
-});
-
 Route::get('plans', function() {
     echo 'Page coming soon. <a href="/">Return to homepage</a>';
 });
