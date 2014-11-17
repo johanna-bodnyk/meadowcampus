439d26c7e6e67fa28ad3825ef30577b98beeac91
Johanna Bodnyk <bodnyk@gmail.com>
2014-11-17 16:25:59 -0500

    javascript background fix

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -16,6 +16,8 @@
         <!-- JQUERY -->
         <script src="{{ URL::asset('packages/jquery/jquery-1.11.1.min.js') }}"></script>
 
+        <script src="{{ URL::asset('js/background-fix.js') }}"></script>
+
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
