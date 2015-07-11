759887b28e05ea9a856c954971b0dceeae5b5465
Johanna Bodnyk <bodnyk@gmail.com>
2015-07-11 19:56:49 -0400

    thermometer fix explanation

--- app/views/fragments/thermometer-head.blade.php
+++ app/views/fragments/thermometer-head.blade.php
@@ -19,6 +19,9 @@
             to {-webkit-clip-path: polygon(0% 0%, {{$percent}}% 0%, {{$percent}}% 100%, 0% 100%);}
         }
 
+{{--    2015-07-11: Chrome now supports unprefixed keyframes directive,
+        but still needs prefix for clip-path, so the following breaks the animation --}}
+
         {{--@keyframes thermometer-image {--}}
             {{--from {clip-path: polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%);}--}}
             {{--to {clip-path: polygon(0% 0%, {{$percent}}% 0%, {{$percent}}% 100%, 0% 100%);}--}}
