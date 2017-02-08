9ea0e197af4e46f668da66fa67823181e5fb33b6
Johanna Bodnyk <bodnyk@gmail.com>
2017-02-07 21:44:24 -0500

    remove learn more link from scenarios page

--- app/views/scenarios.blade.php
+++ app/views/scenarios.blade.php
@@ -14,7 +14,7 @@
 
 @section('content')
     <h2>Monthly Donation Scenarios &amp; Calculators</h2>
-    <p class="lead">A monthly pledge of $64.40, continued for 10 years (with 5% interest), will net the school $10,000. <a href="/help">Learn more about the project and the campaign.</a></p>
+    <p class="lead">A monthly pledge of $64.40, continued for 10 years (with 5% interest), will net the school $10,000.</p>
     <div class="row">
         <div class="col-md-6">
             <h3>Monthly Amounts by Target Gift</h3>
